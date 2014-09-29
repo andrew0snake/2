d13f029d60cf8cc00bdb12e48e164ae884b69a62
Andrew from Mac <andrew0000snake@gmail.com>
2014-09-29 23:06:12 +0700

    20140929 first commit

---
+++ 2.01/2.01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <limits.h>
+
+void main()
+{
+    printf("CHAR_BIT=%d\n",CHAR_BIT);
+    printf("SCHAR_MIN=%d\n",SCHAR_MIN);
+    printf("SCHAR_MAX=%d\n",SCHAR_MAX);
+    printf("UCHAR_MAX=%d\n",UCHAR_MAX);
+    printf("INT_MAX=%d\n",INT_MAX);
+    printf("INT_MIN=%d\n",INT_MIN);
+    printf("LONG_MIN=%d\n",LONG_MIN);
+    printf("LONG_MAX=%d\n",LONG_MAX);
+    printf("SHRT_MIN=%d\n",SHRT_MIN);
+    printf("SHRT_MAX=%d\n",SHRT_MAX);
+    printf("UCHAR_MAX=%d\n",UCHAR_MAX);
+    printf("UINT_MAX=%d\n",UINT_MAX);
+    printf("USHRT_MAX=%d\n",USHRT_MAX);
+    printf("ULONG_MAX=%d\n",ULONG_MAX);
+}
---
+++ git_link.sh
@@ -0,0 +1,4 @@
+git pull git@github.com:andrew0snake/2.git
+git push git@github.com:andrew0snake/2.git master
+
+
