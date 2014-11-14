de6032cde12d3d12dc246663863b300fa7739d1b
Andrew from Mac <andrew0000snake@gmail.com>
2014-11-15 00:09:11 +0600

    20141115 maybe done

--- 2.01/2.01.c
+++ 2.01/2.01.c
@@ -13,8 +13,8 @@ unsigned int unsigned_int_max();
 int int_min();
 int int_max();
 
-long calculate_ulint_max();
-long calculate_long_min();
+unsigned long ulong_max();
+long min_size_long();
 long max_size_long();
 
 int calculate_llong_min();
@@ -25,7 +25,8 @@ int main()
 {
     int char_d,schar_min_d,schar_max_d,uchar_max_d,int_min_d,int_max_d,short_min_d,shirt_max_d,uint_max_d,i;
     unsigned int unsigned_int;
-    long long_min,long_max,ulong_max_d;
+    long long_min,long_max;
+    unsigned long u_long_max;
     long long llong_min_d,llong_max_d,ullong_max_d;
 
     printf("\n\nThis defined in file limits.h variables are just taken from file.\n");
@@ -57,8 +58,8 @@ int main()
     printf("INT_MIN=%d\n",INT_MIN);
     printf("INT_MAX=%d\n",INT_MAX);
     printf("UINT_MAX=%u\n",UINT_MAX);
-    printf("LONG_MAX=%ld\n",LONG_MAX);
-    printf("LONG_MIN=%ld\n",LONG_MIN);
+    printf("LONG_MAX= %ld\n",LONG_MAX);
+    printf("LONG_MIN= %ld\n",LONG_MIN);
     printf("ULONG_MAX=%lu\n",ULONG_MAX);
     printf("LLONG_MAX=%lld\n",LLONG_MAX);
     printf("LLONG_MIN=%lld\n",LLONG_MIN);
@@ -110,18 +111,15 @@ int main()
 
 //---------------------------------------------long-integer
 
-//    printf("\nNext we will know size of unsigned long int.\n");    
-    long_max=max_size_long();
+    long_max = max_size_long();
     printf("Maximal value of signed long int equal %ld\n",long_max);
 
-//    printf("\nNext we will know size of signed long int.\n");    
-    i=calculate_lint_min();
-    printf("Minimal value of signed long int equal %d\n",i);
+    long_min = min_size_long();
+    printf("Minimal value of signed long int equal %ld\n",long_min);
 
-/*    printf("\nNext we will know size of signed short int.\n");    
-    i=calculate_lint_max();
-    printf("Maximal value of signed long int equal %d\n",i);
-*/
+    u_long_max = ulong_max();
+    printf("Maximal value of signed long int equal %lu\n",u_long_max);
+
 }
 
 
@@ -272,6 +270,7 @@ int int_min()
             d = d + step;
         }
         d = d_p;
+
     }
     return d;
 }
@@ -346,3 +345,60 @@ long max_size_long()
     return d;
 }
 
+long min_size_long()
+{
+
+    long d,d_p,step;
+    step = d = d_p = 0;
+
+    d = (-1);
+    while ( d<0 ){
+        d_p = d;
+        d = d*10;
+    }
+    d = d_p;
+
+    step = d;
+    while ( step<(-1) ){
+        step = step/10;
+        while ( d<0 ){
+            d_p = d;
+            d = d+step;
+        }
+        d = d_p;
+    }
+
+//    printf ( "d=%ld;\n",d );
+    return d;
+
+}
+
+unsigned long ulong_max(){
+
+    int prov;
+    unsigned long d,d_p,step;
+
+    d = d_p = step = 0;
+    d = 1;
+    while ( d>d_p ){
+        d_p = d;
+        d = d*10;
+    }
+    d = d_p;
+
+    step = d;
+    while ( step>1 ){
+        step = step/10;
+        prov = 0;
+        d_p = 0;
+        while ( prov==0 ) {
+            if ( (d+step)<d )
+                prov = 1;
+            else
+                d = d+step;
+        }
+//        d = d_p;
+    }
+    return d;
+
+}
