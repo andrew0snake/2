ba01a9e537744b702b23a0a5cb01acd8c550acfb
Andrew from Mac <andrew0000snake@gmail.com>
2014-12-30 01:20:46 +0600

    20141230 any done

--- 2.05/any.c
+++ 2.05/any.c
@@ -4,10 +4,7 @@ void squezze ( char s[], int c, int len );
 int getline_my ( char line[] );
 void strcat_my ( char s[], char t[] );
 void squezze_del ( char s1[], char s2[] );
-void array1 ( char ar1[] );
-void array2 ( char ar1[] );
-void checking (char ar1[] );
-
+char any ( char s1[], char s2[] );
 
 void main ()
 {
@@ -34,25 +31,23 @@ void main ()
     }
 */    
 //    printf ( "And now we try to concatenate strings.\nPlease, input first string:" );
-//    printf ( "Please, input first string:" );
-//    len = getline_my ( first );
-//    printf ( "Please, input second string:" );
-//    len = getline_my ( second );
+    printf ( "Please, input first string:" );
+    len = getline_my ( first );
+    printf ( "Please, input second string:" );
+    len = getline_my ( second );
+
 //    squezze_del ( first, second );
-//    array1 ( first );
-//    array2 ( first );
-//    checking ( first );
 //    printf ( "after check string seems so:%s;\n", second );
+
 //    printf ( "Before concatenation strings seems so:\nFirst string:%s;\nSecond string:%s;\n", first, second );
 //    strcat_my ( first, second );
 //    printf ( "Copying first string to second.\nResult:%s;\n", first );
+    sum = any ( first, second );
+    if ( sum == -1 )
+        printf ( "We do not get any matches.\n" );
+    else
+        printf ( "After applying function any we get returned value of position = %d; and value of array = %c;\n", sum, first[sum] );
 
-    i = 1;
-    j = 2;
-    sum = i | j;
-    printf ( "i = %d; j = %d;\ni & j = %d; i | j = %d;\n", i, j, i & j, i | j);
-
-    
 /*    i = 5;
     j = 3;
     sum = ++i;
@@ -131,62 +126,42 @@ void squezze_del ( char s1[], char s2[] )
     int j = 0;
     int k = 0;
 
-    for (i = 0; s2[i] != '\0'; ++i) {
-        if ( s2[i] == 'c' ) {
-            j = i;
-            while ( s2[j] != '\0' ) {
-                printf ( "s2[j=%d]=%c;",j,s2[j] );
-                s2[j] = s2[j+1];
-                printf ( "s2[j=%d]=%c;\n",j,s2[j] );
-//                getchar ();
-                ++j;
+    for (i = 0; s1[i] != '\0'; ++i) {
+        for ( j = 0; s2[j] != '\0'; ++j) {
+            if ( s2[j] == s1[i]) {
+                k = j;
+                while ( s2[k] != '\0' ) {
+                    s2[k] = s2[k+1];
+                    ++k;
+                };
+            };            
+        };
+    };
+};
+
+
+char any ( char s1[], char s2[] ) 
+{
+
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    char check1 = 0;
+
+    for ( i = 0; ( s1[i] != '\0' ) && ( check1 == 0 ); ++i){
+        for ( j = 0; ( s2[j] != '\0' ) && ( check1 == 0); ++j ){
+            if ( s1[i] == s2[j] ) {
+                check1 = 1;
+
             };
         };
     };
+    
+    if ( check1 == 1 )
+        k = i-1;
+    else
+        k = -1;
+
+    return k;
 }
 
-void array1 (char ar1 [] )
-{
-
-    int i = 0;
-    int prov = 0;
-
-    for ( i = 0; prov == 0; ++i ){
-        printf ( "before check prov = %d; i = %d; ar1[%d] = %c; ", prov, i, i, ar1[i] );
-        if ( ar1[i] == 'c' ) 
-            prov = 1;
-        printf ( "after check prov = %d;\n", prov );
-    };
-
-    printf ( "after check in function with for we have i = %d; ar[%d] = %c;\n", i, i, ar1[i] );
-
-
-}
-
-void array2 (char ar1 [] )
-{
-
-    int i = 0;
-    int prov = 0;
-
-    while ( prov == 0 ){
-        if ( ar1[i] == 'c' ) 
-            prov = 1;
-        ++i;
-    };
-
-    printf ( "after check in function with while we have i = %d; ar[%d] = %c;\n", i, i, ar1[i] );
-
-
-}
-
-
-void checking ( char ar[] )
-{
-    int i = 0;
-
-    while ( ar[i] != '\0') {
-        printf ( "ar[%i] = %c; and in dec=%d; ar[%d+1] = %c; and in dec = %d; ar[%d] & ar[%d] = %d;\n", i, ar[i], ar[i], i, ar[i+1], ar[i+1], i, i+1, ar[i] & ar[i+1] );
-        ++i;
-    };
-}
