7a37cb173f327a07a5b00996c8591c190740e508
Andrew from rosbt <help@rosbt.com>
2015-02-04 12:04:53 +0600

    20150204 inverted

--- 2.07/invert.c
+++ 2.07/invert.c
@@ -14,6 +14,7 @@ void main ()
     unsigned short int n = 0;
     unsigned short int p = 0;
     unsigned short int setb = 0;
+    unsigned short int inv = 0;
     unsigned short int x = 0;
     unsigned short int y = 0;
 
@@ -27,14 +28,11 @@ void main ()
     printf ( "n = %d;\np = %d;\nx = %d;\ny = %d;\n", n, p, x, y );
     printf ( "n in binary = %36d;\np in binary = %36d;\nx in binary = %36d;\ny in binary = %36d;\n\n", dec_bin ( n ), dec_bin ( p ), dec_bin ( x ), dec_bin ( y ) );
 
-    setb = setbits ( x, p, n, y );
-    printf ( "rezult = %4d; and in binary = %19d;\n", setb, dec_bin ( setb ) );
+//    setb = setbits ( x, p, n, y );
+//    printf ( "rezult = %4d; and in binary = %19d;\n\n", setb, dec_bin ( setb ) );
 
-    setb = setbits2 ( x, p, n, y );
-    printf ( "rezult 2 = %2d; and in binary = %19d;\n\n", setb, dec_bin ( setb ) );
-    
-    y = 65535;
-    printf ( "y = %d; and in bin = %d; and in oct = %o;\n", y, dec_bin ( y ), y );
+    inv = invert ( x, p, n );
+    printf ( "rezult of inv = %d; and in binary = %12d;\n", inv, dec_bin ( inv ) );
 
 }
 
@@ -112,12 +110,18 @@ int setbits2 ( int x, int p, int n, int y )
     return fourth;
 }
 
+
+
+
 unsigned getbits ( unsigned x, int p, int n )
 {
 
     return ( x >> ( p+1-n ) & ~ ( ~ 0 << n) );
 
 }
+
+
+
 
 int dec_bin ( int n )  /* Function to convert decimal to binary.*/
 {    
@@ -134,6 +138,9 @@ int dec_bin ( int n )  /* Function to convert decimal to binary.*/
     return binary;
 }
 
+
+
+
 int binary_decimal ( int n ) /* Function to convert binary to decimal.*/
 {
     int decimal = 0;
@@ -148,4 +155,37 @@ int binary_decimal ( int n ) /* Function to convert binary to decimal.*/
     }
 
     return decimal;
+}
+
+
+unsigned short int invert ( unsigned short int x, unsigned short int p, unsigned short int n )
+{
+    unsigned short int first = 0;
+    unsigned short int second = 0;
+    unsigned short int third = 0;
+    unsigned short int fourth = 0;
+    unsigned short int fifth = 0;
+
+    //get n ones bits on position, that starts in p
+    first = (  ( ~ ( ~ 0 << n ) << ( p - n + 1 ) ) );
+    printf ( "first = %42d;\n", dec_bin ( first ) );
+
+    //get n bits of x, that are on position p
+    second = ( x & ( ( ~ ( ~ 0 << n) ) << ( p - n + 1 ) ) );
+    printf ( "second = %41d;\n", dec_bin ( second ) );
+    
+    //get nulls in n bits of x in position, that starts in p
+    third = x & ( ~ first ) ;
+    printf ( "third = %42d;\n", dec_bin ( third ) );
+
+    //get inverse of n bits of x from position p
+    fourth = first & ( ~ second );
+    printf ( "fourth = %41d;\n\n", dec_bin ( fourth ) );
+    
+    //put inverse bits in nulled on position p x
+    fifth = third | fourth;
+    printf ( "fifth = %42d;\n ", dec_bin ( fifth ) ); 
+
+    return second;
+
 }
