15b8a0061624be0bde9d1328e784a22793890a5e
Andrew from Mac <andrew0000snake@gmail.com>
2015-02-18 23:59:17 +0600

    20150219

---
+++ test_dir/operands.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main ()
+{
+    int c = 0;
+
+//    c = 125;
+    printf ( "insert please c:" );
+    scanf ( "%d", &c );
+
+    c -= 5;
+    printf ( "c -=5 = %d;\n", c );
+
+    c += 10;
+    printf ( "c += 10 = %d;\n", c );
+
+    c *=2;
+    printf ( "c *=2 = %d;\n", c );
+
+    c /= 2;
+    printf ( "c /= 2 = %d;\n", c );
+ 
+    c <<=3;
+    printf ( "c <<= 3 = %d;\n", c );
+
+
+    c >>=2;
+    printf ( "c >>= 2 = %d;\n", c );
+
+
+    c %= 2;
+    printf ( "c %= 2 = %d;\n", c );
+
+
+}
